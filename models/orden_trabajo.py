1aced8a10cb553d5f92d2cd8ba76b18baf50959b
mgah77 <mauricio.gah@gmail.com>
2023-11-23 18:59:55 -0300

    Update orden_trabajo.py

--- models/orden_trabajo.py
+++ models/orden_trabajo.py
@@ -47,7 +47,7 @@ class Taller_ot_line(models.Model):
     cant = fields.Integer(string = 'Cantidad', default = 1)
     fecha_entr = fields.Date('Fecha de Entrega', compute="_compute_fecha_entrega")
     nave = fields.Char('Nave', compute="_compute_nave")
-    depto = fields.Char('depto', default="N", compute='_compute_depto', store=True)
+    depto = fields.Char('depto', default='New')
 
 
     def _compute_fecha_entrega(self):
@@ -64,3 +64,11 @@ class Taller_ot_line(models.Model):
         for line in self:
             line['depto'] = line.item.depto
         return
+    
+    @api.model
+    def create(self,vals):       
+        if vals.get('depto','New')=='New':
+            for line in self:
+                vals['depto'] = line.item.depto                        
+        result = super(Taller_ot_line,self).create(vals)
+        return result
