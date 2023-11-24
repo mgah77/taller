c81d97c559726c32b55118565e9397c0753b9b0e
mgah77 <mauricio.gah@gmail.com>
2023-11-24 14:56:39 -0300

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -4,4 +4,4 @@ class ProductDepartment(models.Model):
     _inherit = 'product.template'  # Reemplaza 'your.model' por el nombre del modelo que deseas modificar
 
     # Modifica el campo partner_id para agregar un filtro adicional
-    depto = fields.Many2one('taller.depto.rel', string='Depto')
+    depto = fields.Many2one('taller.depto.rel', string='Depto', readonly=True)
