18ad9a49e70703eff6e81143e5e6844ad3ecdeac
mgah77 <mauricio.gah@gmail.com>
2023-11-24 14:58:35 -0300

    3

--- models/maestro.py
+++ models/maestro.py
@@ -4,4 +4,4 @@ class Maestro_depto(models.Model):
     _name = 'taller.depto.rel'
     _description = 'taller deptos'
 
-    name = fields.Char('depto', default='New')
+    name = fields.Char('depto', default='New', readonly=True)
