6b418e170562d497635234f81dcbc61aae2b5919
mgah77 <mauricio.gah@gmail.com>
2023-11-24 14:02:54 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,7 @@
 'data': ['security/groups.xml',
          'security/ir.model.access.csv',
          'views/menu.xml',
+         'views/maestro.xml',
          'views/orden_trabajo.xml',
          'views/planificacion.xml'
 ],
