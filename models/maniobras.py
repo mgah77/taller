19e86c17367b1ea73661e9097818bca9e3804cea
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-01-10 10:09:57 -0300

    123

---
+++ models/maniobras.py
@@ -0,0 +1,41 @@
+from odoo import models, fields , api , _
+import datetime
+
+class Taller_ingreso(models.Model):
+
+    _name = 'taller.maniobras'
+    _description = 'Maniobras Taller'
+
+    name = fields.Char(string="Nro ", readonly=True, default='New', copy=False)
+    user_branch = fields.Integer(string = 'Current Branch')
+    fecha = fields.Date('Fecha', index=True)
+    horario = fields.Selection([
+        ("am","AM"),
+        ("pm","PM"),
+        ("ap","AM/PM"),
+        ("xc","X Confirmar")], defaut="xc"
+    )
+    nave = fields.Char('Nave')
+    obs = fields.Char('Observaciones')
+    sucursal = fields.Char('Sucursal', compute="_compute_sucursal")
+    horario = fields.Selection([
+        ("pen","Pendiente"),
+        ("rea","Realizado"),
+        ("can","Cancelado")], defaut="pen"
+    )
+
+    @api.model
+    def create(self,vals):
+        if vals.get('name','New')=='New':
+            vals['name']=self.env['ir.sequence'].next_by_code('abr.ot') or 'New'
+            vals['user_branch']=self.env.user.property_warehouse_id
+        result = super(Taller_ingreso,self).create(vals)
+        return result
+
+    
+    def _compute_sucursal(self):
+        for line in self:
+            if line.user_branch == 2:
+                line.sucursal = 'Ñuble'
+            elif line.user_branch == 3:
+                line.sucursal = 'Par Vial'    
