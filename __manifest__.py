6cd672e2c3db9f58ecdf6413e2542b4aa0c0ce40
mgah77 <mauricio.gah@gmail.com>
2024-02-01 12:54:16 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 'author': "Mauricio Gah",
 'license': "AGPL-3",
 'application': "True",
-'version': "2.0",
+'version': "2.1",
 'data': ['security/groups.xml',
          'security/ir.model.access.csv',
          'views/plan_xls.xml',
@@ -15,8 +15,12 @@
          'views/product_template.xml',
          'views/orden_trabajo.xml',
          'views/planificacion.xml',
-         'views/maniobras.xml'
-         
+         'views/maniobras.xml'         
 ],
+'assets': {
+    'web.assets_backend': [
+        'taller/static/src/js/action_manager.js',
+    ],
+},
 'depends': ['base' , 'contacts' , 'stock' , 'product', 'parches_insumar']
 }
