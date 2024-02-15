6a833984da6e03eff90749c859797e3690a3dba5
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-02-15 11:07:33 -0300

    Update plan_xls.py

--- models/plan_xls.py
+++ models/plan_xls.py
@@ -40,26 +40,20 @@ class taller_plan2xls(models.TransientModel):
         categ_id = data.mapped('id')
         if categ_id:
             categ_products = self.env['product.product'].search([('categ_id', 'in', categ_id)])
-
         else:
             categ_products = self.env['product.product'].search([])
-        product_ids = tuple([pro_id.id for pro_id in categ_products])
         for obj in categ_products:
             sale_value = 0
             purchase_value = 0
             vals = {
-                'sku': obj.default_code,
                 'name': obj.name,
-                'category': obj.categ_id.name,
-                'cost_price': obj.standard_price,
-                'available': available_qty,
-                'virtual': virtual_available,
-                'incoming': incoming_qty,
-                'outgoing': outgoing_qty,
-                'net_on_hand': obj.with_context({'warehouse': warehouse_ids}).qty_available,
-                'total_value': value,
-                'sale_value': sale_value,
-                'purchase_value': purchase_value,
+                'armador': obj.armador,
+                'depto': obj.depto,
+                'fecha': obj.fecha,
+                'nave': obj.nave,
+                'detalle': obj.alias,
+                'status': obj.state,
+                'color': obj.color,
             }
             lines.append(vals)
         return lines
