199d6b7b419587145d79bb5f228c7f7ff35d1769
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-02-19 10:30:20 -0300

    Revert "Update plan_xls.py"
    This reverts commit a1d8cff0a7fe80fd76ac16e090a514e02a5b97dd.

--- models/plan_xls.py
+++ models/plan_xls.py
@@ -34,32 +34,3 @@ class taller_plan2xls(models.TransientModel):
                      },
             'report_type': 'stock_xlsx'
         }
-
-    def get_lines(self, data):
-        lines = []
-        categ_id = data.mapped('id')
-        if categ_id:
-            categ_products = self.env['product.product'].search([('categ_id', 'in', categ_id)])
-
-        else:
-            categ_products = self.env['product.product'].search([])
-        product_ids = tuple([pro_id.id for pro_id in categ_products])
-        for obj in categ_products:
-            sale_value = 0
-            purchase_value = 0
-            vals = {
-                'sku': obj.default_code,
-                'name': obj.name,
-                'category': obj.categ_id.name,
-                'cost_price': obj.standard_price,
-                'available': available_qty,
-                'virtual': virtual_available,
-                'incoming': incoming_qty,
-                'outgoing': outgoing_qty,
-                'net_on_hand': obj.with_context({'warehouse': warehouse_ids}).qty_available,
-                'total_value': value,
-                'sale_value': sale_value,
-                'purchase_value': purchase_value,
-            }
-            lines.append(vals)
-        return lines
