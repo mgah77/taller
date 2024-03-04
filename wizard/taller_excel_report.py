95754f6ac801d1c9d1ddcadd247958048369c12a
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-03-04 11:23:30 -0300

    Update taller_excel_report.py

--- wizard/taller_excel_report.py
+++ wizard/taller_excel_report.py
@@ -40,8 +40,8 @@ class ExcelWizard(models.TransientModel):
        }
    def get_xlsx_report(self, data, response):
         partners = self.env['taller.ot.line'].search([('state','=','tall')])
-        balsas = self.env['taller.ot.line'].search([('depto.name','=','Inspeccion Balsas'),('branch','=','viewer')], order="fecha asc")
-        conten = self.env['taller.ot.line'].search([('depto.name','=','Contenedores'),('branch','=','viewer')], order="fecha asc")
+        balsas = partners.search([('depto.name','=','Inspeccion Balsas'),('branch','=','viewer')], order="fecha asc")
+        conten = partners.search([('depto.name','=','Contenedores'),('branch','=','viewer')], order="fecha asc")
 
         # Create Excel workbook and worksheet
         output = io.BytesIO()
