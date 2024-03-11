91ebe4e20cadc1cca959f86d1759950f99535e20
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-03-11 17:56:53 -0300

    Update taller_excel_report.py

--- wizard/taller_excel_report.py
+++ wizard/taller_excel_report.py
@@ -107,9 +107,9 @@ class ExcelWizard(models.TransientModel):
 
         # Write data
         for row, balsas in enumerate(balsas, start=5):
-            worksheet.write(row, 1, balsas.name)
-            worksheet.write(row, 2, str(balsas.fecha.strftime("%d-%m-%y")))
-            worksheet.write(row, 3, balsas.nave)
+            worksheet.write(row, 1, balsas.name, format2)
+            worksheet.write(row, 2, str(balsas.fecha.strftime("%d-%m-%y")), format2)
+            worksheet.write(row, 3, balsas.nave, format2)
             worksheet.write(row, 4, " ")
 
         for row, conten in enumerate(conten, start=5):
