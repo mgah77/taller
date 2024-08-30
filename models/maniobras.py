485ccd120eff42edd331d46fa9c7a4ae9e1c7930
mgah77 <mauricio.gah@gmail.com>
2024-08-30 14:28:57 -0400

    Update maniobras.py

--- models/maniobras.py
+++ models/maniobras.py
@@ -61,7 +61,7 @@ class Taller_ingreso(models.Model):
                 'show_as': 'busy',
                 'description': self.obs,
                 'active': 'True',
-                'start_date' : '',                
+                'start_date' : self.fecha,                
                 'allday': 'False',
                 'start': datet,
                 'stop' : dafin
