54f80d431c0027a93f68ef3bbf662ba541dc8355
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-06 12:30:44 -0300

    34

--- models/reemplazos.py
+++ models/reemplazos.py
@@ -20,8 +20,8 @@ class EntregaEquipos(models.Model):
         if vals.get('name', 'Nuevo') == 'Nuevo':
             vals['name'] = self.env['ir.sequence'].next_by_code('entrega.equipos') or 'Nuevo'
         return super(EntregaEquipos, self).create(vals)
-        
-    def action_print_report(self):
+
+    def action_print_entregas(self):
         return self.env.ref('entrega_equipos.action_report_entrega_equipos').report_action(self)
 
 class EntregaEquiposLine(models.Model):
