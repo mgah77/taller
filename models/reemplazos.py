4251451e85a763134429274734f796f3b3f6a909
mgah77 <mauricio.gah@gmail.com>
2025-02-20 13:22:06 -0300

    sdf

--- models/reemplazos.py
+++ models/reemplazos.py
@@ -18,6 +18,7 @@ class EntregaEquipos(models.Model):
     ], string='Estado', default='borrador')
     sucursel = fields.Selection([('2','Ñuble'),('3','Par Vial')],string='Sucursal',default='2')
     viewer = fields.Integer('Current User', compute="_compute_viewer")
+    responsable = fields.Many2one('res.users', string='Responsable', default=lambda self: self.env.user, readonly=True, default="Sala de Ventas")
 
     def _compute_viewer(self):
         for record in self:
