e953cb089b17973022e9043530bb59eaf9994c3d
mgah77 <mauricio.gah@gmail.com>
2025-06-04 16:54:49 -0400

    se agrega rut a ot

--- models/orden_trabajo.py
+++ models/orden_trabajo.py
@@ -12,6 +12,7 @@ class Taller_ingreso(models.Model):
     fecha_recep = fields.Date('Fecha de Recepción', default=fields.Date.context_today)
     fecha_entr = fields.Date('Fecha de Entrega', index=True)
     armador = fields.Many2one('res.partner',string='Armador',domain="[('type', '!=', 'private'), ('is_company', '=', True), ('type','=','contact'), ('is_customer','=',True)]")
+    armador_rut = fields.Char(string="RUT", related='armador.document_number', readonly=True, store=True)
     nave = fields.Char('Nave',default='MN ')
     obs = fields.Html('Observaciones')
     ot_line = fields.One2many(comodel_name = 'taller.ot.line',inverse_name = 'ot_line_id', string = 'Lineas OT',copy=True, auto_join=True)
