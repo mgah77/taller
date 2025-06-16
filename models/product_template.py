076aea49508e84cced733f0a5bc396d7c9cf7fd3
mgah77 <mauricio.gah@gmail.com>
2025-06-16 11:03:54 -0400

    Update product_template.py

--- models/product_template.py
+++ models/product_template.py
@@ -8,10 +8,3 @@ class ProductDepartment(models.Model):
     alias = fields.Char('Detalle')
 
     
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        # Filtrar por la bodega del usuario si está definida
-        user_warehouse = self.env.user.property_warehouse_id
-        if user_warehouse:
-            args += [('warehouse_id', '=', user_warehouse.id)]
-        return super(ProductTemplate, self).search(args, offset=offset, limit=limit, order=order, count=count)
